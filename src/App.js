68222748c09e82d5e34e7d83ba0850dae2106a0c
lirioblanko <goldliliya@mail.ru>
2022-06-03 14:43:18 +0300

    Layout (Header, Footer, Main)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import {Header} from "./layout/Header";
+import {Footer} from "./layout/Footer";
+import {Main} from "./layout/Main";
 
 function App() {
   return (
     <div className="App">
+        <Header />
+        <Main />
+        <Footer />
     </div>
   );
 }
---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header () {
+    return <nav className='green lighten-1'>
+        <div className="nav-wrapper container">
+            <a href="#" className="brand-logo">E-Commerce</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="">Repo</a></li>
+            </ul>
+        </div>
+    </nav>
+}
+
+export { Header }
