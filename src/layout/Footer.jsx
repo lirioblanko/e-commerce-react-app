1337d61f3c590b6874396d02cadc8264cead0220
lirioblanko <goldliliya@mail.ru>
2022-06-07 14:22:34 +0300

    repo

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,7 +3,7 @@ function Footer () {
         <div className="footer-copyright">
             <div className="container">
                 © { new Date().getFullYear()} E-commerce project. Lirioblanko
-                <a className="grey-text text-lighten-4 right" href="#">Repo</a>
+                <a className="grey-text text-lighten-4 right" href="https://github.com/lirioblanko/e-commerce-react-app">Repo</a>
             </div>
         </div>
     </footer>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,7 +3,7 @@ function Header () {
         <div className="nav-wrapper container">
             <a href="#" className="brand-logo">E-Commerce</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="">Repo</a></li>
+                <li><a href="https://github.com/lirioblanko/e-commerce-react-app">Repo</a></li>
             </ul>
         </div>
     </nav>
